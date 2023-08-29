9e2625026b0a7feed4c4fed421a0f96544ceb681
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-08-29 20:27:01 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "home" })
+
+  get("/rock", { :controller => "application", :action => "rock" })
+
+  get("/paper", { :controller => "application", :action => "paper" })
+
+  get("/scissors", { :controller => "application", :action => "scissors" })
 end
