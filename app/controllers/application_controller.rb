dc89b2b88b293b1d4b8600ac4727c38ed94034fc
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-08-29 20:27:11 +0000

    created actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+  def home
+    render({ :template => "game_templates/home" })
+  end
+
+  def rock
+    render({ :template => "game_templates/rock" })
+  end
+
+  def paper
+    render({ :template => "game_templates/paper" })
+  end
+
+  def scissors
+    render({ :template => "game_templates/scissors" })
+  end
 end
